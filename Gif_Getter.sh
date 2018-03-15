1d09b21376b7e048da1d7f7a4c95b73a76f7ea6d
Nick Volkov <aid13@mail.ru>
2018-03-15 15:11:08 +0300

    Create initial version of Gif_Getter. Create abilitiy to download random gif from giphy.com to current directory.

---
+++ Gif_Getter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#download gifs to current directory
+
+# giphy api_key
+API_KEY='Ialawd1YX6Apj02SPiSxcqeZ9qVYGE1E'
+
+# main path
+GIPHY_HOST='api.giphy.com'
+
+function downloadRandomGif() {
+  # download random gif into the current directory
+  CURRENT_PATH='/v1/gifs/random'
+  curl -o temp.txt $GIPHY_HOST$CURRENT_PATH?api_key=$API_KEY
+  wget -O "$(jq -r '.data.title' temp.txt).gif" $(jq -r '.data.image_url' temp.txt)
+  # rm temp.txt
+  unset CURRENT_PATH
+}
+
+downloadRandomGif
