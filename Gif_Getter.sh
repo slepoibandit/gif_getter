47ff76beb75d128bb7b74f08c75f44b3373a4895
Nick Volkov <aid13@mail.ru>
2018-03-15 19:24:46 +0300

    Add support for additional tag parameter.

--- Gif_Getter.sh
+++ Gif_Getter.sh
@@ -10,10 +10,10 @@ GIPHY_HOST='api.giphy.com'
 function downloadRandomGif() {
   # download random gif into the current directory
   CURRENT_PATH='/v1/gifs/random'
-  curl -o temp.txt $GIPHY_HOST$CURRENT_PATH?api_key=$API_KEY
+  curl -o temp.txt "$GIPHY_HOST$CURRENT_PATH?api_key=$API_KEY&tag=$1"
   wget -O "$(jq -r '.data.title' temp.txt).gif" $(jq -r '.data.image_url' temp.txt)
-  # rm temp.txt
+  rm -f temp.txt
   unset CURRENT_PATH
 }
 
-downloadRandomGif
+downloadRandomGif $1
